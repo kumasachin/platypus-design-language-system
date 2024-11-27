129801a7b3e3908ab10b39d37d0dc53203330798
sachin <kumasachin@gmail.com>
2024-11-27 22:54:30 +0000

    export provider

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -4,3 +4,5 @@ import { StyledEngineProvider } from '@mui/styled-engine'
 const Provider = ({ Children, storybook }: { storybook?: boolean; Children: React.ReactNode }) => (
   <StyledEngineProvider>{Children}</StyledEngineProvider>
 )
+
+export default Provider
